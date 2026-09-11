3afe813e632ec96eeaefca8cc5e4b2d1b32e23e9
Miguel Lima <mglima1279@gmail.com>
2026-09-11 14:35:50 -0300

    a

--- ProJ/ProJ/Program.cs
+++ ProJ/ProJ/Program.cs
@@ -44,12 +44,4 @@ if (!app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
-app.UseRouting();
-
-app.UseAuthorization();
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
-
 app.Run();
